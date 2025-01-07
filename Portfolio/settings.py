1c7c79f967bb4485fc2718a91a5b96e6373470e4
Amir Sarrafzadeh Arasi <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2025-01-07 18:30:58 +0100

    Clear cache added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = [".vercel.app", "*"]
 
 INSTALLED_APPS = [
     'home',
+    'clearcache',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
