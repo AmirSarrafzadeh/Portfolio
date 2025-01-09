63769b503e18a3f9eae55acb95d64cfefde6452f
Amir Sarrafzadeh Arasi <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2025-01-09 09:59:42 +0100

    Update `date +'%Y/%m/%d %H:%M:%S'`

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "*"]
+ALLOWED_HOSTS = ["herokuapp.com"]
 
 # Application definition
 
